537b9d8d477fcfde601df04df48193645a150f81
mayze2001 <143843001+mayze2001@users.noreply.github.com>
2023-10-04 19:05:41 +0200

    Add files via upload
    laatste versie code

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
-// JavaScript Document
-console.log("hi");
+function playSound() {
+    var audio = new Audio(src="images/heartsound.mp3"); // geluid als je op het hartje klikt, het eerste hartje tot nu toe
+    audio.play();
+}
+
+// <!-- Maya Coelho 2023 -->
