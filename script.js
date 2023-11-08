6a6d50eebeb8faa1e40e9f566c7b44cd897de8e0
mayze2001 <143843001+mayze2001@users.noreply.github.com>
2023-11-08 21:45:43 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-function playSound() {
-    var audio = new Audio(src="images/heartsound.mp3"); // geluid als je op het hartje klikt, het eerste hartje tot nu toe
-    audio.play();
+function addToFavorites(heartElement) {
+    // Create a new message element
+    const message = document.createElement('div');
+    message.textContent = 'Toegevoegd aan favorieten';
+    message.className = 'message';
+
+    // Append the message to the body element to display it at the top right corner
+    document.body.appendChild(message);
+
+    // Position the message at the top right corner
+    message.style.position = 'fixed';
+    message.style.top = '20px'; // Adjust the vertical position
+    message.style.right = '20px'; // Adjust the horizontal position
+
+    // Set a timeout to remove the message after a few seconds
+    setTimeout(function() {
+        message.remove();
+    }, 3000); // Adjust the time (in milliseconds) the message is displayed
 }
 
 // <!-- Maya Coelho 2023 -->
